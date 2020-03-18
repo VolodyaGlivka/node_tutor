01679dfdd09b43ba98474d1e4ed68f385a52985d
volodya19971 <mega-volodya.glivka@ukr.net>
2020-03-18 13:47:31 +0200

    remove-node_modules-folder-v1

--- index.js
+++ index.js
@@ -2,25 +2,14 @@ const express = require('express');
 
 const app = express();
 
-// function that we passed will be executed for every incoming request
-// next allow us to continue and use anouther middleware
 app.use((req, res, next) => {
-  console.log('hello');
   next();
 });
 
-// with next() in previous use() we get access to execute anouther middleware
-app.use((req, res, next) => {
-  console.log('world');
-  // here we need to send responce as we don't have next in this function
-  res.send('hello world');
-});
-
-// we don't reach this code
-// as previous middleware don't allow us to execute next line of code
 app.get('/', (req, res) => {
-  const uri = req.url;
-  console.log('uri', uri);
+  const url = req.url;
+  console.log('url', url);
+  res.send({ url });
 });
 
 app.listen(8000);
